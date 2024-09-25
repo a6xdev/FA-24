6271fe6f4fef125ed16d765fb7a6ac3a9f681247
a6xdev <aa16.principal@gmail.com>
2024-09-25 18:46:50 -0300

    hud controller

--- scripts/Gameplay/VehicleController/UI/HudController.gd
+++ scripts/Gameplay/VehicleController/UI/HudController.gd
@@ -5,9 +5,13 @@ class_name HudController
 
 @export_group("HUD Controller")
 @export var GearLabel:Label
+@export var rpmLabel:Label
+@export var SpeedLabel:Label
 
 func _physics_process(delta: float) -> void:
 	GearHUD()
+	rpmHUD()
+	SpeedHud()
 	
 func GearHUD():
 	if BodyNode.current_gear == -1:
@@ -16,3 +20,9 @@ func GearHUD():
 		GearLabel.text = "N"
 	else:
 		GearLabel.text = str(BodyNode.current_gear)
+		
+func rpmHUD():
+	rpmLabel.text = str(BodyNode.current_rpm)
+
+func SpeedHud():
+	SpeedLabel.text = str(BodyNode.current_speed)
