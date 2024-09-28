8b91a7fda9947157fecc13f631efcbc7b17095ce
a6xdev <aa16.principal@gmail.com>
2024-09-28 20:49:50 -0300

    add RPM UI

--- scripts/Gameplay/VehicleController/UI/HudController.gd
+++ scripts/Gameplay/VehicleController/UI/HudController.gd
@@ -2,11 +2,21 @@ extends Node
 class_name HudController
 
 @export var BodyNode:BodyController
+@export var VehicleEngine:EngineController
 
 @export_group("HUD Controller")
 @export var GearLabel:Label
 @export var rpmLabel:Label
 @export var SpeedLabel:Label
+@export var COLOR_RECT:ColorRect
+
+var current_state:state = state.WHITE
+enum state {
+	WHITE,
+	GREEN, # ponto ideal para performance.
+	RED, # Atenção que o limite está perto
+	BLUE # Fudeu
+}
 
 func _physics_process(delta: float) -> void:
 	GearHUD()
@@ -23,6 +33,19 @@ func GearHUD():
 		
 func rpmHUD():
 	rpmLabel.text = str(BodyNode.current_rpm)
+	
+	if  VehicleEngine.rpm >= 14000:
+		current_state = state.BLUE
+		COLOR_RECT.modulate = Color('#003aff')
+	elif VehicleEngine.rpm >= 12000:
+		current_state = state.RED
+		COLOR_RECT.modulate = Color('#ff0000')
+	elif VehicleEngine.rpm >= 10000:
+		current_state = state.GREEN
+		COLOR_RECT.modulate = Color('#04ff00')
+	else:
+		current_state = state.WHITE
+		COLOR_RECT.modulate = Color('#ffffff')
 
 func SpeedHud():
 	SpeedLabel.text = str(BodyNode.current_speed)
