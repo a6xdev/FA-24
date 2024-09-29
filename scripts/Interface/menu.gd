3d1dbfa45f6ef0e46bfafe895b06e1743a10b407
a6xdev <aa16.principal@gmail.com>
2024-09-29 19:12:57 -0300

    new script: Test Menu

--- scripts/Interface/menu.gd
+++ scripts/Interface/menu.gd
@@ -5,5 +5,6 @@ func _input(event: InputEvent) -> void:
 		$VBoxContainer2.visible = !$VBoxContainer2.visible
 
 func _process(delta: float) -> void:
-	$VBoxContainer2/VBoxContainer/label.text = "Sensitivity: " + str($VBoxContainer2/VBoxContainer/HSlider.value)
+	$VBoxContainer2/VBoxContainer/label.text = "Steering Sensitivity: " + str($VBoxContainer2/VBoxContainer/SteerSensi.value)
 	$VBoxContainer2/VBoxContainer2/label.text = "Deadzone: " + str($VBoxContainer2/VBoxContainer2/HSlider.value)
+	$VBoxContainer2/VBoxContainer3/label.text = "Sensitivity Accel: " + str($VBoxContainer2/VBoxContainer3/AccelSensi.value)
