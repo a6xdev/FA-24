eabbade45b45a3c90dc382b2bf72a3aa6f7aa43e
a6xdev <aa16.principal@gmail.com>
2024-09-29 19:10:00 -0300

    script global for save player settings

---
+++ scripts/core/global/PlayerConfig.gd
@@ -0,0 +1,55 @@
+extends Node
+
+var save_path = "res://data/"
+
+# Configurações do jogador local
+var steer_sensitivity:float = 1.0
+var accelerometer_sensitivity:float = 1.0
+var volume:float = 1.0
+var screen_resolution:Vector2 = Vector2(1920, 1080)
+var audio_settings = {
+	"music_volume": 1.0,
+	"engine_volume": 1.0,
+	"tire_volume": 1.0
+}
+
+# Configurações do carro
+var ERS:bool = false
+var ABS:bool = false
+var TractionControl:bool = false
+
+# <------------------------------------------------------------------------->
+# Data
+# <------------------------------------------------------------------------->
+
+func save_config():
+	var file = FileAccess.open(save_path, FileAccess.WRITE)
+	file.store_var({
+		"steer_sensitivity": steer_sensitivity,
+		"accelerometer_sensitivity": accelerometer_sensitivity,
+		"volume": volume,
+		"screen_resolution": screen_resolution,
+		"audio_settings": audio_settings
+	})
+
+func load_config():
+	if FileAccess.file_exists(save_path):
+		var file = FileAccess.open(save_path, FileAccess.READ)
+		var data = file.get_var()
+		
+		steer_sensitivity = data.get("steer_sensitivity", 1.0)
+		accelerometer_sensitivity = data.get("accelerometer_sensitivity, 1.0")
+		volume = data.get("volume", 1.0)
+		screen_resolution = data.get("screen_resolution", Vector2(1920, 1080))
+		audio_settings = data.get("audio_settings", {
+			"music_volume": 1.0,
+			"engine_volume": 1.0,
+			"tire_volume": 1.0
+		})
+	else:
+		print("No data saved. Sorry litle driver :<")
+		steer_sensitivity = 0
+		accelerometer_sensitivity = 0
+		volume = 0
+		screen_resolution = Vector2(0,0)
+		audio_settings = 0
