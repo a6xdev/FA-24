c2edb193d7aa4932fd04db280bfd6bc7a2227023
a6xdev <aa16.principal@gmail.com>
2024-09-29 19:10:31 -0300

    new script: SoundsManager

---
+++ scripts/gameplay/audio/Vehicle/SoundsManager.gd
@@ -0,0 +1,19 @@
+extends Node
+class_name VehicleSoundsManager
+
+@export var VehicleEngine:EngineController
+@export var min_pitch = 0.8
+@export var max_pitch = 1.5
+
+@export_group("Sounds")
+@export var EngineAudio:AudioStreamPlayer
+	
+func _ready() -> void:
+	EngineAudio.play()
+
+func _physics_process(delta: float) -> void:
+	EngineSound()
+	
+func EngineSound():
+	var rpm_ratio = VehicleEngine.rpm / VehicleEngine.max_rpm
+	EngineAudio.pitch_scale = lerp(min_pitch, max_pitch, rpm_ratio)
