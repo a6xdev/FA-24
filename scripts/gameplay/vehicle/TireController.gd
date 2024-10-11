5c7bd1b9ce80a69dcd91f9ba8c50d893c9bbdf06
a6xdev <aa16.principal@gmail.com>
2024-10-11 19:55:06 -0300

    feat: added tire screech audio (test)

--- scripts/gameplay/vehicle/TireController.gd
+++ scripts/gameplay/vehicle/TireController.gd
@@ -1,6 +1,7 @@
 extends VehicleWheel3D
 
 @export var PlayerConfigNode:PlayerConfig
+@export var DeslizePneu:float = 0.6
 
 @export_group("Grip")
 @export var max_grip: float = 3.0  # Aderência máxima do pneu
@@ -10,10 +11,19 @@ extends VehicleWheel3D
 @export var temperature: float = 70.0  # Temperatura inicial do pneu
 @export var max_temperature: float = 120.0  # Temperatura máxima que o pneu pode alcançar
 
+@export_category("Tire Audio Controller")
+@export var TireScreechAudio:AudioStreamPlayer
+
 var current_grip: float = max_grip  # Aderência atual do pneu
 var wear: float = 0.0  # Nível de desgaste do pneu
 
 var TractionControl:bool = false
 
 func _physics_process(delta: float) -> void:
+	if TireScreechAudio:
+		if get_skidinfo() <= DeslizePneu:
+			TireScreechAudio.play()
+		else:
+			TireScreechAudio.stop()
+		
 	wheel_friction_slip = max_grip
