460a711a7e5e3ddfa8fe137a8cdfec4283e0d7ba
a6xdev <aa16.principal@gmail.com>
2024-10-13 09:18:40 -0300

    Revert "feat: dynamic camera"
    This reverts commit e2ec841fbe572e13dd34988b53bcb79700e6fb88.

--- scripts/gameplay/UI/SettingsScreen.gd
+++ scripts/gameplay/UI/SettingsScreen.gd
@@ -2,11 +2,6 @@ extends Control
 
 @export var PlayerConfigNode:PlayerConfig
 
-var is_game_paused:bool = false
-
-@export_group("Interface Adicionais")
-@export var CarUI:Control
-
 # Controls
 @onready var steer_slider: HSlider = $TabContainer/Controls/MarginContainer/VBoxContainer/SteerSensititivy/SteerSlider
 @onready var steer_value: Label = $TabContainer/Controls/MarginContainer/VBoxContainer/SteerSensititivy/Value
@@ -18,32 +13,13 @@ var is_game_paused:bool = false
 @onready var traction_control: CheckBox = $TabContainer/Difficulty/MarginContainer/VBoxContainer/TractionControl/CheckBox2
 @onready var automatic_gear: CheckBox = $TabContainer/Difficulty/MarginContainer/VBoxContainer/AutomaticGear/CheckBox2
 
-func _input(event: InputEvent) -> void:
-	if event.is_action_released("ui_cancel"):
-		is_game_paused = !is_game_paused
-		
-#func _ready() -> void:
-	#self.visible = false
-
-# <---------------------------------------------------->
-# <---------------------------------------------------->
-# <---------------------------------------------------->
 func _process(delta: float) -> void:
-	if is_game_paused:
-		get_tree().paused = true
-		self.visible = true
-		CarUI.visible = false
-	else:
-		get_tree().paused = false
-		self.visible = false
-		CarUI.visible = true
-		
 	# Controls
 	PlayerConfigNode.steer_sensitivity = steer_slider.value
 	steer_value.text = str(steer_slider.value)
 	PlayerConfigNode.accelerometer_sensitivity = accel_slider.value
 	accel_value.text = str(accel_slider.value)
-
+	
 	
 	# Difficulty
 func ABS_toggled(toggled_on: bool) -> void:
@@ -52,9 +28,3 @@ func TractionControl_toggled(toggled_on: bool) -> void:
 	PlayerConfigNode.TractionControl = !PlayerConfigNode.TractionControl
 func automaticGear_toggled(toggled_on: bool) -> void:
 	PlayerConfigNode.automatic_gear = !PlayerConfigNode.automatic_gear
-
-func _on_back_toggled(toggled_on: bool) -> void:
-	is_game_paused = false
-	get_tree().paused = false
-	self.visible = false
-	CarUI.visible = true
